cb2a9ec91b40fdbba1dfc686fd6d6260d3fcb6d0
mikisi37 <67510204+mikisi37@users.noreply.github.com>
2020-11-02 19:04:54 +0900

    Add files via upload

---
+++ c++言語/コンパイル.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  char a[] = "C:\\MinGW\\bin\\g++.exe -o ";
+  char s[] = " ";
+  char e[] = ".cpp";
+  printf("Do you want the file name to be the same?[y/n]");
+  int f = getchar();
+  if(f == 'y')
+  {
+    printf("Please specify the file name  ");
+    char f[256];
+    scanf("%s",f);
+    strcat(a,f);
+    strcat(a,s);
+    strcat(a,f);
+    strcat(a,e);
+    FILE *pipe = popen(a, "r");
+    pclose(pipe);
+    main();
+  }
+  else if(f == 'n')
+  {
+    printf("Please specify the file name  ");
+    char f[256];
+    scanf("%s",f);
+    printf("Decide on a name for the exe  ");
+    char fe[256];
+    scanf("%s",fe);
+    strcat(a,fe);
+    strcat(a,s);
+    strcat(a,f);
+    strcat(a,e);
+    FILE *pipe = popen(a, "r");
+    pclose(pipe);
+    main();
+  }
+  else if(f == 'f')
+  {
+  }
+}
