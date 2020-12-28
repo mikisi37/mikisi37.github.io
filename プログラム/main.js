7d6eb60e1ed5e888eb7a8af1d5c7dbaad7735194
mikisi37 <67510204+mikisi37@users.noreply.github.com>
2020-12-28 17:51:13 +0900

    Add files via upload

--- プログラム/main.js
+++ プログラム/main.js
@@ -1,5 +1,32 @@
+var xy;
+
+function order(what){
+  if(what == 'up'){
+    document.getElementById(xy).style.transform ="rotate("+270+"deg)";
+    what = '➝';
+  }
+  else if(what == 'down'){
+    document.getElementById(xy).style.transform ="rotate("+90+"deg)";
+    what = '➝';
+  }
+  else if(what == 'right'){
+    document.getElementById(xy).style.transform ="rotate("+0+"deg)";
+    what = '➝';
+  }
+  else if(what == 'left'){
+    document.getElementById(xy).style.transform ="rotate("+180+"deg)";
+    what = '➝';
+  }
+  else{
+    document.getElementById(xy).style.transform ="rotate("+0+"deg)";
+  }
+  document.documentElement.style.setProperty("--order","none");
+  document.getElementById(xy).value = what;
+}
+
 function main(where){
-  document.getElementById(where).value="aaaaa";
+  document.documentElement.style.setProperty("--order","block");
+  xy = where;
 }
 
 window_siz();
