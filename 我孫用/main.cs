ec0748d41be6bb41d200aca840de6ea9bad68050
mikisi37 <67510204+mikisi37@users.noreply.github.com>
2021-06-17 00:33:32 +0900

    Add files via upload

--- 我孫用/main.cs
+++ 我孫用/main.cs
@@ -3,12 +3,21 @@ using System.IO;
 using System.Text;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 //グローバル関数
 public class Global{
+  public static string Initial = "00000000000000000000000000012000000210000000000000000000000000001";
+  public static string Server = @"C:\Users\Mikisi\Documents\Program\c++\C#\";
+  public static string SaveP = "test.txt";
+  public static string ServerF;
   public static string Account;
-  public static char MyT;
+  public static string List;
+  public static char NowT;
+  public static char MyT = '1';
+  public static char YoT = '2';
   public static bool GameS = false;
+  public static List<int> ListT = new List<int>();
 }
 
 //メイン
@@ -17,7 +26,9 @@ class Program{
     if(!System.IO.File.Exists("Account.txt")){
       Sub.MakeA();
     }
-    Global.Account = File.Read("Account.txt");
+
+    Global.Account = File.Read("Account.txt").TrimEnd('\r', '\n');
+    Sub.StartU();
     Application.Run(new Form1());
   }
 }
@@ -48,6 +59,8 @@ class Form1 : Form{
   void MClick(object sende,MouseEventArgs e){
     Console.WriteLine("{0},{1}",e.X,e.Y);
     Global.GameS = true;
+    Global.List = File.Read(Global.SaveP);
+    //Play.Put(29);
   }
 
   //描画処理
@@ -83,15 +96,27 @@ class Form1 : Form{
       int CountT = 0;
       int CountB = 0;
       int CountW = 0;
-      string Date = File.Read("test.txt");
-
+      Global.List = File.Read(Global.SaveP);
+      Global.NowT = Global.List[64];
+      Global.ListT.Clear();
       RoundG = FieldS * 10 /75;
 
+      //置ける場所特定
+      for(int i = 0;i < 64;i++){
+        if(Global.List[i] == '1' && Global.MyT == '2'){
+          Play.PutT(i);
+        }
+        else if(Global.List[i] == '2' && Global.MyT == '1'){
+          Play.PutT(i);
+        }
+      }
+
+      //駒、置ける場所表示
       for(int y = 0; y < 8; y++){
         for(int x = 0; x < 8; x++){
           int Sx = x *FieldS +GapW +10;
           int Sy = y *FieldS +GapH +10;
-          char Status = Date[x +y *8];
+          char Status = Global.List[x +y *8];
 
           Rectangle rect = new Rectangle(Sx, Sy, FieldS, FieldS);
           Rectangle round = new Rectangle(Sx +RoundG, Sy +RoundG, FieldS -RoundG *2, FieldS -RoundG *2);
@@ -99,24 +124,37 @@ class Form1 : Form{
           g.FillRectangle(brushF,rect);
 
           if(Status == '1'){
-            if(Date[64] == Global.MyT){
-              g.FillEllipse(brushT,Sx +RoundG *2, Sy +RoundG *2, FieldS -RoundG *4, FieldS -RoundG *4);
-              CountT++;
-            }
-          }
-          else if(Status == '2'){
             g.FillEllipse(brushB,round);
             CountB++;
           }
-          else if(Status == '3'){
+          else if(Status == '2'){
             g.FillEllipse(brushW,round);
             CountW++;
+          }
+          else if(Global.ListT.Contains(x +y *8) && Global.MyT == Global.NowT){
+            g.FillEllipse(brushT,Sx +RoundG *2, Sy +RoundG *2, FieldS -RoundG *4, FieldS -RoundG *4);
+            CountT++;
           }
         }
       }
 
-      //情報表示
+      //置ける場所がないとき
+      if(CountT == 0){
+        List<string> ListN = new List<string>(){};
+        foreach(char Copy in Global.List){
+          ListN.Add(Copy.ToString());
+        }
+        ListN[64] = Global.YoT.ToString();
+        File.Write(Global.SaveP, String.Join("",ListN.ToArray()));
+      }
+
+      //勝敗
       Count = CountB + CountW;
+      if(CountB == 0 || CountW == 0 || Count == 64){
+        Global.GameS = false;
+      }
+
+      //情報表示
       Font font = new Font("Times New Roman", RoundG *3 /2, FontStyle.Regular);
       string text = string.Format("石：総数{0}　黒{1},　白{2}",Count,CountB,CountW);
       g.DrawString(text,font,brushW, 0, 0);
@@ -129,6 +167,68 @@ class Form1 : Form{
     brushB.Dispose();
     penB.Dispose();
 
+  }
+}
+
+class Play{
+  static List<int> Cons = new List<int>(){-9,-8,-7,-1,1,7,8,9};
+
+  //置ける場所特定
+  public static void PutT(int Place){
+    foreach(int i in Cons){
+      int Place1 = Place;
+      int Place2 = Place + i;
+
+      if(Global.List[Place2] == '0'){
+        while(true){
+          if(Place1 < 0 && Place1 > 64 || Global.List[Place1] == '0'){
+            break;
+          }
+          else if(Global.List[Place1] == Global.MyT){
+            Global.ListT.Add(Place2);
+            break;
+          }
+
+          Place1 -= i;
+        }
+      }
+    }
+  }
+
+  //駒をひっくり返す処理
+  public static void Put(int Place){
+    List<string> ListN = new List<string>(){};
+    foreach(char Copy in Global.List){
+      ListN.Add(Copy.ToString());
+    }
+    ListN[Place] = Global.MyT.ToString();
+
+    foreach(int i in Cons){
+      int Place1 = Place + i;
+      var PutO = new List<int>(){};
+
+      if(Global.List[Place1] == Global.YoT){
+        while(true){
+          if(Global.List[Place1] == '0'){
+            break;
+          }
+          else if(Global.List[Place1] == Global.MyT){
+            foreach(int ii in PutO){
+              ListN[ii] = Global.MyT.ToString();
+            }
+            break;
+          }
+          else{
+            PutO.Add(Place1);
+          }
+
+          Place1 += i;
+        }
+      }
+    }
+
+    ListN[64] = Global.YoT.ToString();
+    File.Write(Global.SaveP, String.Join("",ListN.ToArray()));
   }
 }
 
@@ -151,5 +251,3 @@ class File{
     writer.Close();
   }
 }
-
-//    Console.WriteLine();
--- 我孫用/sub.cs
+++ 我孫用/sub.cs
@@ -1,10 +1,16 @@
 using System;
+using System.IO;
 using System.Drawing;
 using System.Windows.Forms;
 
 class Sub{
   public static void MakeA(){
     Application.Run(new Form2());
+  }
+
+  public static void StartU(){
+    File.Write(Global.Server + Global.Account + @"\Server.txt", Global.Initial);
+    File.Write(Global.Server + Global.Account + @"\Data.txt", "1");
   }
 }
 
@@ -17,6 +23,7 @@ class Form2 : Form{
     this.StartPosition = FormStartPosition.CenterScreen;
     this.MinimumSize = new Size(500,300);
     this.MaximumSize = new Size(500,300);
+    this.Icon = new System.Drawing.Icon("main.ico");
     this.Text = "アカウント作成";
 
     label.Text = "アカウント名を設定してください。";
@@ -40,7 +47,8 @@ class Form2 : Form{
 
   void button_Click(object sender,EventArgs e){
     if(textbox.Text != ""){
-      File.Write("Account.txt",textbox.Text);
+      File.Write("Account.txt", textbox.Text);
+      Directory.CreateDirectory(Global.Server + textbox.Text);
       this.Close();
     }
   }
