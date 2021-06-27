11852b96ea84f2e437924c92ec85f5abd21b2f7a
mikisi37 <67510204+mikisi37@users.noreply.github.com>
2021-06-27 18:46:13 +0900

    Add files via upload

---
+++ 我孫用/警告/main.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class Program{
+  static void Main(){
+    Application.Run(new Form1());
+  }
+}
+
+class Form1 : Form{
+  PictureBox pictureBox1 = new PictureBox();
+
+  public Form1(){
+    this.StartPosition = FormStartPosition.CenterScreen;
+    this.FormBorderStyle = FormBorderStyle.None;
+    this.TransparencyKey = this.BackColor;
+    this.ShowInTaskbar = false;
+    this.Width = 640;
+    this.Height = 500;
+
+    Controls.Add(pictureBox1);
+
+    this.pictureBox1.Dock = DockStyle.Fill;
+    Bitmap bm = new Bitmap("main.gif");
+    this.pictureBox1.Image = bm;
+  }
+}
